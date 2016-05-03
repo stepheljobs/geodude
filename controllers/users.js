881fc1fefc5b791afddafe083d86a515e865ee95
stepheljobs <stephelgeorgemaca@gmail.com>
2016-05-03 12:09:34 +0800

    Added user controller.

---
+++ controllers/users.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var Redis = require('ioredis');
+var psubLocation = require('../service/psublocation');
+
+function Users(req, cb){
+  var db = new Redis({port: 6379,host: '127.0.0.1'});
+
+  switch (req.route.action) {
+    case 'brokers_profile': // {"route": { "module":"user", "action": "brokers_profile" } , "payload": { "userid": "z9PS4Kgz", "working_email": "stephelbroker1@gmail.com", "contact_number": "09054866990", "brokerlisc": "12345", "yrexam": "2006", "cover_areas": "Makati,Fort Bonifacio" }}
+      console.log('brokers_profile: ');
+      // id and details.
+      var userid = req.payload.userid;
+      var userProfile = {
+        working_email: req.payload.working_email,
+        contact_number: req.payload.contact_number,
+        brokerlisc: req.payload.brokerlisc,
+        yrexam: req.payload.yrexam,
+        cover_areas: req.payload.cover_areas
+      }
+
+      db.hmset("hm-user."+userid, userProfile);
+      db.hgetall("hm-user."+userid, function(err, user) {
+        console.log('user: ', JSON.stringify(user));
+        cb("success", user);
+
+        // subscribe to a location.
+        var areas = user.cover_areas.split(",");
+        areas.map(function(area){
+          psubLocation(area);
+        });
+
+      });
+      break;
+    default:
+  }
+}
+
+module.exports = Users;
