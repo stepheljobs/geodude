ae797bbbbc651739bd27eee54108ac127aca7b81
stepheljobs <stephelgeorgemaca@gmail.com>
2016-05-03 16:34:09 +0800

    Try catch the request to catch the bug.

--- socket.js
+++ socket.js
@@ -18,31 +18,37 @@ function Socket(conn) {
 
   conn.on('data', function(message) {
       console.log('message: ', message);
-      var req = JSON.parse(message);
 
-      switch (req.route.module) {
-        case "auth":
-          authController(req, function(status,data){
-            var response = { "response" : { "code": status, route: {"module": req.route.module, "action": req.route.action }, "payload": data } }
-            console.log('response: ', JSON.stringify(response));
-            conn.write(JSON.stringify(response));
-          });
-          break;
-          case "request":
-            requestController(req, function(status,data){
+      try {
+        var req = JSON.parse(message);
+
+        switch (req.route.module) {
+          case "auth":
+            authController(req, function(status,data){
               var response = { "response" : { "code": status, route: {"module": req.route.module, "action": req.route.action }, "payload": data } }
               console.log('response: ', JSON.stringify(response));
               conn.write(JSON.stringify(response));
             });
             break;
-          case "user":
-            userController(req, function(status,data){
-              var response = { "response" : { "code": status, route: {"module": req.route.module, "action": req.route.action }, "payload": data } }
-              console.log('response: ', JSON.stringify(response));
-              conn.write(JSON.stringify(response));
-            });
-            break;
-        default:
+            case "request":
+              requestController(req, function(status,data){
+                var response = { "response" : { "code": status, route: {"module": req.route.module, "action": req.route.action }, "payload": data } }
+                console.log('response: ', JSON.stringify(response));
+                conn.write(JSON.stringify(response));
+              });
+              break;
+            case "user":
+              userController(req, function(status,data){
+                var response = { "response" : { "code": status, route: {"module": req.route.module, "action": req.route.action }, "payload": data } }
+                console.log('response: ', JSON.stringify(response));
+                conn.write(JSON.stringify(response));
+              });
+              break;
+          default:
+        }
+      } catch(err) {
+        console.log("err: ", err);
+        conn.write("Invalid format");
       }
 
   });
