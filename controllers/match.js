4cdfd71d6bd73cb0d5cdcb8db9469a53772d9672
stepheljobs <stephelgeorgemaca@gmail.com>
2016-05-10 09:15:37 +0800

    Initial commit.

--- controllers/match.js
+++ controllers/match.js
@@ -2,6 +2,7 @@
 
 var Redis = require('ioredis');
 var pubRequest = require('../service/pubrequest');
+var fetchAllMatch = require('../service/fetchallmatch');
 
 function Match(req, cb) {
   var db = new Redis({port: 6379,host: '127.0.0.1'});
@@ -9,13 +10,21 @@ function Match(req, cb) {
   switch (req.route.action) {
     case 'haveit':
         pubRequest(req.payload, function(err, result){
-          if(result){
+          if (result) {
             cb("success", result);
-          }else{
+          } else {
             cb("error", "No response.");
           }
         });
       break;
+    case 'fetchallmatch':
+        fetchAllMatch(req.payload.clientid, function(result){
+          if (result) {
+            cb("success", result);
+          } else {
+            cb("error", "No response.");
+          }
+        });
       break;
     default:
   }
---
+++ service/fetchallmatch.js
@@ -0,0 +1,9 @@
+'use strict'
+
+var Redis = require('ioredis');
+
+function FetchAllMatch(clientid, cb) {
+  
+}
+
+module.exports = FetchAllMatch;
