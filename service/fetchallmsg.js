c1623dc147f3ea148173284449cb75d084ee3013
stepheljobs <stephelgeorgemaca@gmail.com>
2016-05-12 18:08:57 +0800

    fetch all message convert string to json.

--- service/fetchallmsg.js
+++ service/fetchallmsg.js
@@ -5,12 +5,17 @@ var Redis = require('ioredis');
 function FetchAllMsg(roomid, cb) {
 
   var db = new Redis({port: 6379, host: '127.0.0.1'});
-
+  var arrayMsg = []
   if (roomid) {
     var min = 0, max = -1;
     db.lrange(roomid, min, max, function (err, messages) {
       console.log(roomid + " messages: ", messages);
-      cb('success', messages);
+      messages.map(function(message, iter, total){
+        arrayMsg.push(JSON.parse(message));
+        if(iter === total.length - 1){
+          cb("success",arrayMsg);
+        }
+      });
     });
   } else {
     cb('invalid', 'no roomid.');
