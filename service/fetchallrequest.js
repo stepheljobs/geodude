f1496b84520d1de7db2ca42080d54f33cf9e1cef
stepheljobs <stephelgeorgemaca@gmail.com>
2016-05-17 15:18:07 +0800

    updated fetch all request.

--- service/fetchallrequest.js
+++ service/fetchallrequest.js
@@ -6,6 +6,7 @@ function FetchAllRequest(blockrequest,cover_areas, cb) {
   var db = new Redis({port: 6379,host: '127.0.0.1'});
   var sortedRequest = [];
   db.keys('hm-req.????????', function(err, requests) {
+
     requests.map(function(singleRequest, iter, total) {
 
       if (blockrequest) {
@@ -16,12 +17,18 @@ function FetchAllRequest(blockrequest,cover_areas, cb) {
               if(requestDetail.area.includes(ca)) {
                 console.log("match cover area: ", requestDetail);
                 sortedRequest.push(requestDetail);
+
+                if(iter === total.length - 1){
+                  console.log('');
+                  cb(null,sortedRequest);
+                }
               }
             });
           });
         } else {
           console.log("a request is blocked already by the broker: ", singleRequest);
         }
+
       } else {
         db.hgetall(singleRequest, function(err, requestDetail) {
           var area = cover_areas.split(",");
@@ -29,15 +36,15 @@ function FetchAllRequest(blockrequest,cover_areas, cb) {
             if(requestDetail.area.includes(ca)) {
               console.log("match cover area: ", requestDetail);
               sortedRequest.push(requestDetail);
+
+              if(iter === total.length - 1){
+                console.log('');
+                cb(null,sortedRequest);
+              }
             }
           });
         });
       }
-
-      if(iter === total.length - 1){
-        cb(null,sortedRequest);
-      }
-      
     });
   });
 }
