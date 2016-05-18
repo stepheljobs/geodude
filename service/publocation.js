9114186c1158f1e4d2fc0d903a9360cb17f3bf31
stepheljobs <stephelgeorgemaca@gmail.com>
2016-05-18 10:25:04 +0800

    Improve with multiple matching.

--- service/publocation.js
+++ service/publocation.js
@@ -3,9 +3,16 @@
 var Redis = require('ioredis');
 
 function PubLocation(reqid, location, requestdetails, cb) {
+  console.log('location ---> ', location);
   var pub = new Redis({port: 6379,host: '127.0.0.1'});
+
+  var areaArray = location.split(",");
+  areaArray.map(function(area){
+      pub.publish(reqid+"."+area, JSON.stringify(requestdetails));
+  });
+
+  // callback for total count of match brokers
   var count = 0;
-  pub.publish(reqid+"."+location, JSON.stringify(requestdetails));
   pub.keys('hm-user.*', function(err, listofusers) {
     listofusers.map(function(user, iter, total) {
       pub.hgetall(user, function(err, details){
@@ -19,6 +26,7 @@ function PubLocation(reqid, location, requestdetails, cb) {
       });
     });
   });
+
 }
 
 module.exports = PubLocation;
