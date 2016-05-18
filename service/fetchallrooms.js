331f79d55594dc06e41beddd609050f0ac8b7b1a
stepheljobs <stephelgeorgemaca@gmail.com>
2016-05-18 11:00:11 +0800

    Fetch all rooms unmatched name fix.

--- service/fetchallrooms.js
+++ service/fetchallrooms.js
@@ -11,31 +11,43 @@ function FetchAllRooms(userid, usertype, cb) {
       if (usertype === "BROKER") {
         db.keys('chatroom.*'+userid, function(err, listofrooms){
           // [ 'chatroom.84NKQxFZ.FjVzcosl.15WaQywp' ]
-
           var arrayRoom = [];
           listofrooms.map(function(room, iter, total){
             var min = 0, max = -1;
-            var splitRoomId = room.split(".",2);
+            var splitRoomId = room.split(".");
             var rooms = {
               roomid: room,
               requestid: splitRoomId[1],
+              clientid: splitRoomId[2],
+              brokerid: splitRoomId[3],
               photo: '',
               fullname: '',
               latestmsg: ''
             }
 
             db.lrange(room, min, max, function (err, messages) {
-              console.log("messages: ", messages[0]);
               rooms.latestmsg = JSON.parse(messages[0]);
-              db.hgetall('hm-user.'+userid, function(err, profile){
-                rooms.photo = profile.photo;
-                rooms.fullname = profile.first_name + " " + profile.last_name;
-                arrayRoom.push(rooms);
 
-                if(iter === total.length - 1){
-                  cb("success",arrayRoom);
-                }
-              });
+              if (userid === rooms.clientid) {
+                db.hgetall('hm-user.'+brokerid, function(err, profile) {
+                  rooms.photo = profile.photo;
+                  rooms.fullname = profile.first_name + " " + profile.last_name;
+                  arrayRoom.push(rooms);
+                  if(iter === total.length - 1) {
+                    cb("success",arrayRoom);
+                  }
+                });
+              } else {
+                db.hgetall('hm-user.'+clientid, function(err, profile) {
+                  rooms.photo = profile.photo;
+                  rooms.fullname = profile.first_name + " " + profile.last_name;
+                  arrayRoom.push(rooms);
+                  if(iter === total.length - 1) {
+                    cb("success",arrayRoom);
+                  }
+                });
+              }
+
             });
           });
 
@@ -70,14 +82,7 @@ function FetchAllRooms(userid, usertype, cb) {
             });
           });
         });
-      } else {
-        cb("invalid", "Undefined usertype");
-      }
-
-      // if client fetch all rooms related to request.
-
-      // if broker fetch all rooms related to brokerid.
-
+      } else { cb("invalid", "Undefined usertype"); }
     } else { cb("invalid", "No usertype"); }
   } else { cb("invalid", "No clientid"); }
 }
