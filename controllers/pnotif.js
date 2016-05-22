411f96a1131266fa2e8bb2e99f7b70f8cfd82af6
stepheljobs <stephelgeorgemaca@gmail.com>
2016-05-22 15:19:22 +0800

    removed the send push notif.

--- controllers/pnotif.js
+++ controllers/pnotif.js
@@ -1,7 +1,6 @@
 'use strict';
 
 var Redis = require('ioredis');
-var sendPushNotif = require('../service/sendpushnotif');
 var saveToken = require('../service/savetoken');
 
 function PushNotif(req, cb) {
@@ -9,11 +8,6 @@ function PushNotif(req, cb) {
   // var db = new Redis({port: 6379,host: '127.0.0.1'});
 
   switch (req.route.action) {
-    case 'sendpushnotif':
-        sendPushNotif(req.payload, function(status, result){
-          cb(status, result);
-        });
-      break;
     case 'savetoken':
         saveToken(req.payload, function(status, result){
           cb(status, result);
